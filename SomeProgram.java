b1fe2ac0350a47ac43ac8fe96ebed5e3e4ad74bc
student38 <student38@pcoip.ki.agh.edu.pl>
2020-01-18 15:13:15 +0100

    Its not a rocket

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,6 +1,14 @@
 public class SomeProgram { 
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
- System.out.println("We will be Git experts, soon.");
+      System.out.println("We will be Git experts, soon.");
     }
+
+    public void manageTheSatellite() {
+         // TODO implement
+    }
+	public void manageTheSatellite() {
+    // TODO implement
+	}
+
 }
